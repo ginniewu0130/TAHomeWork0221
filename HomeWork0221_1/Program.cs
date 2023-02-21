4a5c77a48af9a83671b0eee083159e460d1ca14c
ginniewu0130 <chutm08@gmail.com>
2023-02-21 11:20:48 +0800

    加入專案檔案。

---
+++ HomeWork0221_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork0221_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //序列倒置Linq
+            //input:12,18,13,23,32 output:32,23,13,18,12
+
+            Console.WriteLine("輸入一串以逗號分隔(,)的字串:");
+            //使用者輸入
+            var input=Console.ReadLine();
+            //分開
+            var inputWords=input.Split(',');
+            //倒置
+            var reverseWords=inputWords.Reverse();
+            //結果輸出
+            var result=string.Join(",", reverseWords);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ HomeWork0221_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork0221_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //奇偶判斷Linq
+            //input: 8,7,9,3,11,6,2,18
+            Console.WriteLine("請輸入一串以逗號分隔(,)的整數數字字串");
+            //輸入
+            var input=Console.ReadLine();
+            //分開
+            var intputWords=input.Split(',');
+            //轉成int
+            var numbers=intputWords.Select((x)=>int.Parse(x));
+            //排列OrderBy()由小到大排列
+            var sortNumbers = numbers.OrderBy((x) => x);
+            //奇數判斷
+            var oddResult=sortNumbers.Where((x)=>x%2!=0);
+            //偶數判斷
+            var evenResult=sortNumbers.Where((x)=>x%2==0);
+            //輸出結果             
+            Console.WriteLine($"奇數：{string.Join(",", oddResult)}");
+            Console.WriteLine($"偶數：{string.Join(",", evenResult)}");
+
+            Console.ReadLine();
+        }
+    }
+}
